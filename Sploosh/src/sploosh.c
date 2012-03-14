34a06d5cb1a1320fd3dc9633e06977efff89d8fe
CJKay <cjkay93@gmail.com>
2012-03-14 18:04:29 +0000

    Set up a basic libmod application

---
+++ Sploosh/src/sploosh.c
@@ -0,0 +1,16 @@
+#include <libmod/libmod.h>
+
+libmod_application_t libmod_application = {
+		{
+				"Sploosh",
+				"The Sploosh bot",
+				{0, 0, 1, 0},
+				{ 0,
+						{ }
+				}
+		}
+};
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
