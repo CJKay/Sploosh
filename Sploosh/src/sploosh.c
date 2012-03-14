55520529b008c48f100ebb51cf8a08f856f3bd45
CJKay <cjkay93@gmail.com>
2012-03-14 18:48:27 +0000

    Added argument check in main()

--- Sploosh/src/sploosh.c
+++ Sploosh/src/sploosh.c
@@ -14,5 +14,12 @@ libmod_application_t libmod_application = {
 };
 
 int main(int argc, char *argv[]) {
+	if(argc < 2 || strcmp(argv[1], "help") == 0) {
+		puts("Usage: sploosh <bot name>");
+		puts("    Runs the Sploosh bot located in `./bots/<bot name>`");
+
+		return 1;
+	}
+
 	return 0;
 }
