c17e45d4cf1c5383d897b3104fbb800bc6c105ff
CJKay <cjkay93@gmail.com>
2012-03-15 18:20:28 +0000

    Added basic prototype functions

--- Sploosh/include/sploosh_log.h
+++ Sploosh/include/sploosh_log.h
@@ -7,4 +7,8 @@ typedef struct sploosh_log {
 	FILE *error;
 } sploosh_log_t;
 
+sploosh_error_t sploosh_log_open(sploosh_log_t *log);
+sploosh_error_t sploosh_log_write(sploosh_log_t *log, const char *format, ...);
+sploosh_error_t sploosh_log_close(sploosh_log_t *log);
+
 #endif // SPLOOSH_LOG_H
