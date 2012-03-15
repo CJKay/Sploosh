747a6959bb8b5a4413cfcbdd2fba418500367099
CJKay <cjkay93@gmail.com>
2012-03-15 19:12:49 +0000

    Removed out and error FILEs

--- Sploosh/src/sploosh_log.c
+++ Sploosh/src/sploosh_log.c
@@ -1,6 +1,7 @@
 #include "../include/sploosh_log.h"
 
-sploosh_error_t sploosh_log_open(sploosh_log_t *log) {
+sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file) {
+	log->file = fopen(file, "wt");
 	return SPLOOSH_NOERROR;
 }
 
@@ -9,5 +10,6 @@ sploosh_error_t sploosh_log_write(sploosh_log_t *log, const char *format, ...) {
 }
 
 sploosh_error_t sploosh_log_close(sploosh_log_t *log) {
+	fclose(log->file);
 	return SPLOOSH_NOERROR;
 }
