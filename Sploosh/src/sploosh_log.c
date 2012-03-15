83ff256fc9d16d4994776a9a0e48a62df7bc7633
CJKay <cjkay93@gmail.com>
2012-03-15 18:24:28 +0000

    Implemented stub functions

---
+++ Sploosh/src/sploosh_log.c
@@ -0,0 +1,13 @@
+#include "../include/sploosh_log.h"
+
+sploosh_error_t sploosh_log_open(sploosh_log_t *log) {
+	return SPLOOSH_NOERROR;
+}
+
+sploosh_error_t sploosh_log_write(sploosh_log_t *log, const char *format, ...) {
+	return SPLOOSH_NOERROR;
+}
+
+sploosh_error_t sploosh_log_close(sploosh_log_t *log) {
+	return SPLOOSH_NOERROR;
+}
