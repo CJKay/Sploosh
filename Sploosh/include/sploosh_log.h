e9855360f13e335f8f2c73fc8d1460d6240c55be
CJKay <cjkay93@gmail.com>
2012-03-15 22:37:08 +0000

    Added very basic open/close functionality

--- Sploosh/include/sploosh_log.h
+++ Sploosh/include/sploosh_log.h
@@ -9,7 +9,7 @@ typedef struct sploosh_log {
 	FILE *file;
 } sploosh_log_t;
 
-sploosh_error_t sploosh_log_open(sploosh_log_t *log);
+sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file);
 sploosh_error_t sploosh_log_write(sploosh_log_t *log, const char *format, ...);
 sploosh_error_t sploosh_log_close(sploosh_log_t *log);
 
--- Sploosh/src/sploosh_log.c
+++ Sploosh/src/sploosh_log.c
@@ -1,15 +1,21 @@
 #include "../include/sploosh_log.h"
 
 sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file) {
-	log->file = fopen(file, "wt");
-	return SPLOOSH_NOERROR;
+	if((log->file = fopen(file, "wt")) == NULL) {
+		perror("Unable to open log");
+		return SPLOOSH_LOG_OPEN_FAILED;
+	}
+
+	return SPLOOSH_NO_ERROR;
 }
 
 sploosh_error_t sploosh_log_write(sploosh_log_t *log, const char *format, ...) {
-	return SPLOOSH_NOERROR;
+	return SPLOOSH_NO_ERROR;
 }
 
 sploosh_error_t sploosh_log_close(sploosh_log_t *log) {
-	fclose(log->file);
-	return SPLOOSH_NOERROR;
+	if(fclose(log->file))
+		return SPLOOSH_LOG_CLOSE_FAILED;
+
+	return SPLOOSH_NO_ERROR;
 }
