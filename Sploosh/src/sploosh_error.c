78046a05cc54b116bfdb819aa2b3135feaf0a300
CJKay <cjkay93@gmail.com>
2012-03-15 21:30:21 +0000

    Renamed errors to suit their descriptions more

--- Sploosh/src/sploosh_error.c
+++ Sploosh/src/sploosh_error.c
@@ -1,13 +1,14 @@
 #include "../include/sploosh_error.h"
 
 static const char *sploosh_errstrs[] = {
-	[SPLOOSH_NOERROR] = "[SPLOOSH_NOERROR] No error",
-	[SPLOOSH_LOGFAILURE] = "[SPLOOSH_LOGFAILURE] Couldn't open log for reading"
+	[SPLOOSH_NO_ERROR] = "[SPLOOSH_NO_ERROR] No error",
+	[SPLOOSH_LOG_OPEN_FAILED] = "[SPLOOSH_LOG_OPEN_FAILED] Couldn't open log for writing",
+	[SPLOOSH_LOG_CLOSE_FAILED] = "[SPLOOSH_LOG_CLOSE_FAILED] Couldn't close log"
 };
 
 const char *sploosh_error_tostring(sploosh_error_t error) {
 	if(error < SPLOOSH_LASTERROR)
 		return sploosh_errstrs[error];
 
-	return sploosh_errstrs[SPLOOSH_NOERROR];
+	return sploosh_errstrs[SPLOOSH_NO_ERROR];
 }
