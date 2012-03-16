d66248774a506437bb408cbf13fc001a99742f57
CJKay <cjkay93@gmail.com>
2012-03-16 10:47:49 +0000

    Added stderr functions sploosh_log_eprintf() and sploosh_log_eputs()

--- Sploosh/src/sploosh_log.c
+++ Sploosh/src/sploosh_log.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file) {
 	if((log->file = fopen(file, "wt")) == NULL) {
@@ -14,7 +15,7 @@ sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file) {
 }
 
 sploosh_error_t sploosh_log_printf(sploosh_log_t *log, const char *tag, const char *format, ...) {
-	char intermediate[256];
+	char intermediate[strlen(format) + 128];
 
 	va_list argp;
 
@@ -22,14 +23,7 @@ sploosh_error_t sploosh_log_printf(sploosh_log_t *log, const char *tag, const ch
 	vsnprintf(intermediate, 256, format, argp);
 	va_end(argp);
 
-	time_t rawtime;
-	struct tm *ptime;
-
-	time(&rawtime);
-	ptime = gmtime(&rawtime);
-
-	printf("[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, intermediate);
-	fprintf(log->file, "[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, intermediate);
+	sploosh_log_puts(log, tag, intermediate);
 
 	return SPLOOSH_NO_ERROR;
 }
@@ -41,8 +35,39 @@ sploosh_error_t sploosh_log_puts(sploosh_log_t *log, const char *tag, const char
 	time(&rawtime);
 	ptime = gmtime(&rawtime);
 
-	printf("[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, str);
-	fprintf(log->file, "[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, str);
+	if(	printf("[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, str) < 0 ||
+		fprintf(log->file, "[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, str
+	) < 0)
+		return SPLOOSH_PRINTF_FAILED;
+
+	return SPLOOSH_NO_ERROR;
+}
+
+sploosh_error_t sploosh_log_eprintf(sploosh_log_t *log, const char *tag, const char *format, ...) {
+	char intermediate[strlen(format) + 128];
+
+	va_list argp;
+
+	va_start(argp, format);
+	vsnprintf(intermediate, 256, format, argp);
+	va_end(argp);
+
+	sploosh_log_eputs(log, tag, intermediate);
+
+	return SPLOOSH_NO_ERROR;
+}
+
+sploosh_error_t sploosh_log_eputs(sploosh_log_t *log, const char *tag, const char *str) {
+	time_t rawtime;
+	struct tm *ptime;
+
+	time(&rawtime);
+	ptime = gmtime(&rawtime);
+
+	if(	fprintf(stderr, "[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, str) < 0 ||
+		fprintf(log->file, "[%02i:%02i:%02i %s] %s\n", ptime->tm_hour, ptime->tm_min, ptime->tm_sec, tag, str
+	) < 0)
+		return SPLOOSH_PRINTF_FAILED;
 
 	return SPLOOSH_NO_ERROR;
 }
