074a2f869addbc104244345f4e85170a49f08abf
CJKay <cjkay93@gmail.com>
2012-03-16 16:54:54 +0000

    Added SPLOOSH_LOG_BUFFERSIZE

--- Sploosh/include/sploosh_log.h
+++ Sploosh/include/sploosh_log.h
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 
+#define SPLOOSH_LOG_BUFFERSIZE 512
+
 typedef struct sploosh_log {
 	FILE *file;
 } sploosh_log_t;
@@ -12,8 +14,8 @@ typedef struct sploosh_log {
 sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file);
 sploosh_error_t sploosh_log_printf(sploosh_log_t *log, const char *tag, const char *format, ...);
 sploosh_error_t sploosh_log_puts(sploosh_log_t *log, const char *tag, const char *str);
-sploosh_error_t sploosh_log_eprintf(sploosh_log_t *log, const char *tag, const char *format, ...);
-sploosh_error_t sploosh_log_eputs(sploosh_log_t *log, const char *tag, const char *str);
+sploosh_error_t sploosh_log_eprintf(sploosh_log_t *log, const char *tag, unsigned int line, const char *file, const char *format, ...);
+sploosh_error_t sploosh_log_eputs(sploosh_log_t *log, const char *tag, unsigned int line, const char *file, const char *str);
 sploosh_error_t sploosh_log_close(sploosh_log_t *log);
 
 #endif // SPLOOSH_LOG_H
