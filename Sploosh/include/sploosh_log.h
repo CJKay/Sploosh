87e59e9019f867f43cfd2bff4d41b04240b3b034
CJKay <cjkay93@gmail.com>
2012-03-17 20:22:19 +0000

    Added sploosh_log_statement_t to logging functions, removing the previous char *tag

--- Sploosh/include/sploosh_log.h
+++ Sploosh/include/sploosh_log.h
@@ -11,11 +11,18 @@ typedef struct sploosh_log {
 	FILE *file;
 } sploosh_log_t;
 
+typedef enum sploosh_log_statement {
+	SPLOOSH_LOG_NOTICE = 0,
+	SPLOOSH_LOG_WARNING,
+	SPLOOSH_LOG_ERROR,
+	SPLOOSH_LOG_CUSTOM
+} sploosh_log_statement_t;
+
 sploosh_error_t sploosh_log_open(sploosh_log_t *log, const char *file);
-sploosh_error_t sploosh_log_printf(sploosh_log_t *log, const char *tag, const char *format, ...);
-sploosh_error_t sploosh_log_puts(sploosh_log_t *log, const char *tag, const char *str);
-sploosh_error_t sploosh_log_eprintf(sploosh_log_t *log, const char *tag, unsigned int line, const char *file, const char *format, ...);
-sploosh_error_t sploosh_log_eputs(sploosh_log_t *log, const char *tag, unsigned int line, const char *file, const char *str);
+sploosh_error_t sploosh_log_printf(sploosh_log_t *log, sploosh_log_statement_t tag, const char *format, ...);
+sploosh_error_t sploosh_log_puts(sploosh_log_t *log, sploosh_log_statement_t tag, const char *str);
+sploosh_error_t sploosh_log_eprintf(sploosh_log_t *log, sploosh_log_statement_t tag, unsigned int line, const char *file, const char *format, ...);
+sploosh_error_t sploosh_log_eputs(sploosh_log_t *log, sploosh_log_statement_t tag, unsigned int line, const char *file, const char *str);
 sploosh_error_t sploosh_log_close(sploosh_log_t *log);
 
 #endif // SPLOOSH_LOG_H
