693827102a0a7d1c44a9997840698adfd179bfeb
CJKay <cjkay93@gmail.com>
2012-03-19 23:06:08 +0000

    Added SPLOOSH_PLUGINS_MAXREACHED

--- Sploosh/src/sploosh_error.c
+++ Sploosh/src/sploosh_error.c
@@ -4,7 +4,8 @@ static const char *sploosh_errstrs[] = {
 	[SPLOOSH_NO_ERROR] = "[SPLOOSH_NO_ERROR] No error",
 	[SPLOOSH_LOG_OPEN_FAILED] = "[SPLOOSH_LOG_OPEN_FAILED] Couldn't open log for writing",
 	[SPLOOSH_LOG_CLOSE_FAILED] = "[SPLOOSH_LOG_CLOSE_FAILED] Couldn't close log",
-	[SPLOOSH_PRINTF_FAILED] = "[SPLOOSH_PRINTF_FAILED] printf() failed"
+	[SPLOOSH_PRINTF_FAILED] = "[SPLOOSH_PRINTF_FAILED] printf() failed",
+	[SPLOOSH_PLUGINS_MAXREACHED] = "[SPLOOSH_PLUGINS_MAXREACHED] Maximum number of plugins reached"
 };
 
 const char *sploosh_error_tostring(sploosh_error_t error) {
