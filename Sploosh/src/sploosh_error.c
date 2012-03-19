140f9586187cb7a7c855ed8ff45819e5834096d1
CJKay <cjkay93@gmail.com>
2012-03-19 23:25:32 +0000

    Added SPLOOSH_PLUGINS_ALREADYLOADED

--- Sploosh/src/sploosh_error.c
+++ Sploosh/src/sploosh_error.c
@@ -6,7 +6,8 @@ static const char *sploosh_errstrs[] = {
 	[SPLOOSH_LOG_CLOSE_FAILED] = "[SPLOOSH_LOG_CLOSE_FAILED] Couldn't close log",
 	[SPLOOSH_PRINTF_FAILED] = "[SPLOOSH_PRINTF_FAILED] printf() failed",
 	[SPLOOSH_PLUGINS_MAXREACHED] = "[SPLOOSH_PLUGINS_MAXREACHED] Maximum number of plugins reached",
-	[SPLOOSH_PLUGINS_NOTINLIST] = "[SPLOOSH_PLUGINS_NOTINLIST] Couldn't find plugin in list"
+	[SPLOOSH_PLUGINS_NOTINLIST] = "[SPLOOSH_PLUGINS_NOTINLIST] Couldn't find plugin in list",
+	[SPLOOSH_PLUGINS_ALREADYLOADED] = "[SPLOOSH_PLUGINS_ALREADYLOADED] Plugin is already loaded"
 };
 
 const char *sploosh_error_tostring(sploosh_error_t error) {
