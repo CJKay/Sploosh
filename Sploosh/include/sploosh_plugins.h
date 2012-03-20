71c8ac253c4258f2272e4fbe7f238b328b4b9b14
CJKay <cjkay93@gmail.com>
2012-03-20 19:16:03 +0000

    Committed after refactoring

--- Sploosh/include/sploosh_plugins.h
+++ Sploosh/include/sploosh_plugins.h
@@ -4,11 +4,14 @@
 #include "sploosh_defs.h"
 #include "sploosh_error.h"
 
+#include <libmod/libmod.h>
+
 typedef struct sploosh_plugins {
 	uint64_t count;
 	libmod_module_t *plugin[SPLOOSH_MAXPLUGINS];
 } sploosh_plugins_t;
 
 sploosh_error_t sploosh_plugins_add(sploosh_plugins_t *list, libmod_module_t *plugin);
+sploosh_error_t sploosh_plugins_remove(sploosh_plugins_t *list, libmod_module_t *plugin);
 
 #endif // SPLOOSH_PLUGINS_H
