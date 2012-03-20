786987174061aab1d3e16c398fd6b550f1453039
CJKay <cjkay93@gmail.com>
2012-03-20 01:05:59 +0000

    Added SPLOOSH_CONFIG_COULDNTLOAD

--- Sploosh/src/sploosh_error.c
+++ Sploosh/src/sploosh_error.c
@@ -7,7 +7,8 @@ static const char *sploosh_errstrs[] = {
 	[SPLOOSH_PRINTF_FAILED] = "[SPLOOSH_PRINTF_FAILED] printf() failed",
 	[SPLOOSH_PLUGINS_MAXREACHED] = "[SPLOOSH_PLUGINS_MAXREACHED] Maximum number of plugins reached",
 	[SPLOOSH_PLUGINS_NOTINLIST] = "[SPLOOSH_PLUGINS_NOTINLIST] Couldn't find plugin in list",
-	[SPLOOSH_PLUGINS_ALREADYLOADED] = "[SPLOOSH_PLUGINS_ALREADYLOADED] Plugin is already loaded"
+	[SPLOOSH_PLUGINS_ALREADYLOADED] = "[SPLOOSH_PLUGINS_ALREADYLOADED] Plugin is already loaded",
+	[SPLOOSH_CONFIG_COULDNTLOAD] = "[SPLOOSH_CONFIG_COULDNTLOAD] Couldn't load config file"
 };
 
 const char *sploosh_error_tostring(sploosh_error_t error) {
