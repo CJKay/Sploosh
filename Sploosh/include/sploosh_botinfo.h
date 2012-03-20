1942ae558165c9fbcb19cae692098e2d43df5ad8
CJKay <cjkay93@gmail.com>
2012-03-20 19:16:14 +0000

    Removed 'name'

--- Sploosh/include/sploosh_botinfo.h
+++ Sploosh/include/sploosh_botinfo.h
@@ -4,13 +4,12 @@
 #include <stdint.h>
 
 typedef struct sploosh_botinfo {
-	const char *name;
 	const char *nickname;
 	const char *username;
 	const char *realname;
 	const char *password;
 	const char *server;
-	const uint16_t port;
+	uint16_t port;
 } sploosh_botinfo_t;
 
 #endif // SPLOOSH_BOTINFO_H
