76a007ea1304bb5f9d8926ad6d3025c95a250fd8
CJKay <cjkay93@gmail.com>
2012-03-20 19:59:18 +0000

    Added log

--- Sploosh/include/sploosh_bot.h
+++ Sploosh/include/sploosh_bot.h
@@ -3,10 +3,12 @@
 
 #include "sploosh_botinfo.h"
 #include "sploosh_plugins.h"
+#include "sploosh_log.h"
 
 typedef struct sploosh_bot {
 	sploosh_botinfo_t info;
 	sploosh_plugins_t plugins;
+	sploosh_log_t log;
 } sploosh_bot_t;
 
 #endif // SPLOOSH_BOT_H
