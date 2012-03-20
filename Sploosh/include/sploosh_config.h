8b3257f2bb857a049a674b55ae96302713b7cc84
CJKay <cjkay93@gmail.com>
2012-03-20 19:58:27 +0000

    First commit of sploosh_config.h

---
+++ Sploosh/include/sploosh_config.h
@@ -0,0 +1,9 @@
+#ifndef SPLOOSH_CONFIG_H
+#define SPLOOSH_CONFIG_H
+
+#include "sploosh_error.h"
+#include "sploosh_bot.h"
+
+sploosh_error_t sploosh_config_import(sploosh_bot_t *bot, const char *cfgfile);
+
+#endif
