fc2d5f3e62ea62c8e0a68624678e48edb9581759
CJKay <cjkay93@gmail.com>
2012-03-25 19:55:41 +0100

    Removed unneccessary forward decls

--- Sploosh/include/sploosh.h
+++ Sploosh/include/sploosh.h
@@ -3,11 +3,6 @@
 
 #include <libmod/libmod.h>
 
-#include <stddef.h>
-
 extern libmod_application_t libmod_application;
-
-void sploosh_printusage(void);
-void sploosh_printversion(void);
 
 #endif /* SPLOOSH_H */
