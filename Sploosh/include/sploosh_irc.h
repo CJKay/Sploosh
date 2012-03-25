c7541fdcdbc513512c97cb9acd466f10bd24b80c
CJKay <cjkay93@gmail.com>
2012-03-25 19:54:28 +0100

    Added forward decls for IRC commands

--- Sploosh/include/sploosh_irc.h
+++ Sploosh/include/sploosh_irc.h
@@ -17,4 +17,14 @@ typedef void (*sploosh_irc_eventcode_callback_t)(unsigned int event, const char 
 typedef void (*sploosh_irc_event_dcc_chat_t)(const char *nick, const char *addr, sploosh_irc_dcc_t dccid);
 typedef void (*sploosh_irc_event_dcc_send_t)(const char *nick, const char *addr, const char *filename, unsigned long size, sploosh_irc_dcc_t dccid);
 
+int sploosh_irc_cmd_join(const char *channel, const char *key);
+int sploosh_irc_cmd_part(const char *channel);
+int sploosh_irc_cmd_invite(const char *nick, const char *channel);
+int sploosh_irc_cmd_names(const char *channel);
+int sploosh_irc_cmd_list(const char *channel);
+int sploosh_irc_cmd_topic(const char *channel, const char *topic);
+int sploosh_irc_cmd_channel_mode(const char *channel, const char *mode);
+int sploosh_irc_cmd_user_mode(const char *mode);
+int sploosh_irc_cmd_kick(const char *nick, const char *channel, const char *reason);
+
 #endif /* SPLOOSH_IRC_H */
