4b2f78fdb7231483ba617e0dc4898bc782159a3d
CJKay <cjkay93@gmail.com>
2012-03-25 18:19:56 +0100

    Added some useful typedefs

--- Sploosh/include/sploosh_irc.h
+++ Sploosh/include/sploosh_irc.h
@@ -11,4 +11,10 @@ typedef struct sploosh_ircinfo_t {
 
 sploosh_error_t sploosh_irc_run(void);
 
+typedef irc_dcc_t sploosh_irc_dcc_t;
+typedef void (*sploosh_irc_event_callback_t)(const char *event, const char *origin, const char **params, unsigned int count);
+typedef void (*sploosh_irc_eventcode_callback_t)(unsigned int event, const char *origin, const char **params, unsigned int count);
+typedef void (*sploosh_irc_event_dcc_chat_t)(const char *nick, const char *addr, sploosh_irc_dcc_t dccid);
+typedef void (*sploosh_irc_event_dcc_send_t)(const char *nick, const char *addr, const char *filename, unsigned long size, sploosh_irc_dcc_t dccid);
+
 #endif /* SPLOOSH_IRC_H */
