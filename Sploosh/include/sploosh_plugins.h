142ba3c0c06109042dfe85e91ff04111189594d8
CJKay <cjkay93@gmail.com>
2012-03-25 19:54:47 +0100

    Added forward decl for sploosh_plugins_clear()

--- Sploosh/include/sploosh_plugins.h
+++ Sploosh/include/sploosh_plugins.h
@@ -7,11 +7,12 @@
 #include <libmod/libmod.h>
 
 typedef struct sploosh_plugins {
-	uint16_t count;
+	unsigned int count;
 	libmod_module_t *plugin[SPLOOSH_MAXPLUGINS];
 } sploosh_plugins_t;
 
-sploosh_error_t sploosh_plugins_add(sploosh_plugins_t *list, libmod_module_t *plugin);
-sploosh_error_t sploosh_plugins_remove(sploosh_plugins_t *list, libmod_module_t *plugin);
+sploosh_error_t sploosh_plugins_add(libmod_module_t *plugin);
+sploosh_error_t sploosh_plugins_remove(libmod_module_t *plugin);
+void sploosh_plugins_clear(void);
 
 #endif /* SPLOOSH_PLUGINS_H */
