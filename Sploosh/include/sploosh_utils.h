6eba57d374339a5f59e3740e1544b4a8683bc478
CJKay <cjkay93@gmail.com>
2012-03-29 17:13:53 +0100

    Removed sploosh_utils_t

--- Sploosh/include/sploosh_utils.h
+++ Sploosh/include/sploosh_utils.h
@@ -3,17 +3,6 @@
 
 #include "snprintf.h"
 
-typedef struct sploosh_utils {
-	int (*snprintf)(char *str, size_t str_m, const char *fmt, ...);
-	int (*vsnprintf)(char *str, size_t str_m, const char *fmt, va_list ap);
-	int (*asprintf)(char **ptr, const char *fmt, ...);
-	int (*vasprintf)(char **ptr, const char *fmt, va_list ap);
-	int (*asnprintf)(char **ptr, size_t str_m, const char *fmt, ...);
-	int (*vasnprintf)(char **ptr, size_t str_m, const char *fmt, va_list ap);
-	int (*strbeginsw)(const char *str, const char *substr);
-	int (*strendsw)(const char *str, const char *substr);
-} sploosh_utils_t;
-
 #define sploosh_utils_snprintf snprintf
 #define sploosh_utils_vsnprintf vsnprintf
 #define sploosh_utils_asprintf asprintf
