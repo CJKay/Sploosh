7ea12934c08db276b7007b76de241e9a72bee2d1
CJKay <cjkay93@gmail.com>
2012-03-29 17:17:23 +0100

    Added missing sploosh_utils_asnprintf

--- Sploosh/include/sploosh_utils.h
+++ Sploosh/include/sploosh_utils.h
@@ -3,10 +3,13 @@
 
 #include "snprintf.h"
 
+#include <string.h>
+
 #define sploosh_utils_snprintf snprintf
 #define sploosh_utils_vsnprintf vsnprintf
 #define sploosh_utils_asprintf asprintf
 #define sploosh_utils_vasprintf vasprintf
+#define sploosh_utils_asnprintf asnprintf
 #define sploosh_utils_vasnprintf vasnprintf
 
 int sploosh_utils_strbeginsw(const char *str, const char *substr) {
